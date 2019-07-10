d4bb0037b9e6f5ae8138b5a286b270ed4a00e407
Devesh Bhor <deveshbhor@outlook.com>
2019-07-10 11:20:46 -0500

    Setup initial Map View

--- Za Hunter/ViewController.swift
+++ Za Hunter/ViewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
